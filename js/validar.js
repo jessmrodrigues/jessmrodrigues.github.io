4afa0b624a7ec2e37eabe4d6311992418ec6c0da
jessmrodrigues <87787892+jessmrodrigues@users.noreply.github.com>
2021-07-21 21:38:06 -0300

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,13 @@
+﻿function valida(){
+	if(document.getElementById("nome").value==""){
+		alert("Preencha o campo nome");
+		documentgetElementById("nome").focus();
+	return false;
+	}
+	if(document.getElementById("password").value.length<7){
+		alert("Informe pelo menos 7 caracteres");
+		document.getElementById("password").focus();
+		return false;
+	}
+	return true;
+}
